c051addd3b48cb652dafd510391e2d9357cfa933
Muhammad Aizaz Haider <106084777+Aizaznaqvi@users.noreply.github.com>
2023-10-21 17:35:01 +0500

    Update and rename Assignment-9 to Assignment9/Solution.ts

--- Assignment9/Solution.ts
+++ Assignment9/Solution.ts
@@ -1,5 +1,3 @@
-Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
-
 const friendsName :string[] = ["Tehreem","Afnan", "Mohsin Late", "Fareed"]
 console.log("Hello" +" "+ friendsName[0] + " " +"How are you?");
 console.log("Hello" +" "+ friendsName[1] + " " +"How are you?");
