7c7e0b55141763bad02341ef60c1e485ae7568c4
Muhammad Aizaz Haider <106084777+Aizaznaqvi@users.noreply.github.com>
2023-10-21 17:08:34 +0500

    Update Solution.ts

--- Assignment7/Solution.ts
+++ Assignment7/Solution.ts
@@ -1,4 +1,2 @@
-console.log(" Addition 4 + 4 =", 4 + 4);
-console.log(" Subtraction 18 - 10 =",18 - 10);
-console.log(" Division 16 / 2 =",16 / 2);
-console.log(" Multiply 4 * 2 =",4 * 2);
+let favoriteNumber = 110;
+console.log(favoriteNumber + " "+ "is Favorite Number")
