88292f95f98b384d1a6af2c09b81631bf786afb0
Muhammad Aizaz Haider <106084777+Aizaznaqvi@users.noreply.github.com>
2023-10-21 17:47:09 +0500

    Update and rename Assignment-12 to Assignment12/Solution.ts

---
+++ Assignment12/Solution.ts
@@ -0,0 +1,11 @@
+const family: string[]= ["Tehreem", "Ghayoor", "Rabis"];
+const invitation:string= "You are invited to Dinner Tonight";
+console.log(family[0]+ " "+ invitation);
+console.log(family[1]+ " "+ invitation);
+console.log(family[2]+ " "+ invitation);
+console.log(family[1]+ " "+ ": I Cannot join due to job");
+
+family[1] = "Safdar Raza";
+console.log(family[0]+ " "+ invitation);
+console.log(family[1]+ " "+ invitation);
+console.log(family[2]+ " "+ invitation);
