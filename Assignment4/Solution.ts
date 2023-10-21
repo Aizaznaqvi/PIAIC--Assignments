489e6d2b1cd55a41a589ac55681c0406d9f1350d
Muhammad Aizaz Haider <106084777+Aizaznaqvi@users.noreply.github.com>
2023-10-21 16:58:40 +0500

    Update and rename Assignment-4 to Assignment4/Solution.ts

---
+++ Assignment4/Solution.ts
@@ -0,0 +1,3 @@
+let famous_person:string = "Zig Ziglar";
+let message:string= "once said \"You don't have to be great to start, but you have to start to be great.\""
+console.log(famous_person +" "+ message)
