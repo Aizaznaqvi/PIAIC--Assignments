028d59e8a7d619cbf228ccacc105fd44f4521de8
Muhammad Aizaz Haider <106084777+Aizaznaqvi@users.noreply.github.com>
2023-10-21 16:54:14 +0500

    Update and rename Assignment-3 to Assignment3/Solution.ts

---
+++ Assignment3/Solution.ts
@@ -0,0 +1,4 @@
+let author:string = "Zig Ziglar";
+let message:string= "once said"
+let quote:string="\"You don't have to be great to start, but you have to start to be great.\"";
+console.log(author +" "+ message+" "+quote)
