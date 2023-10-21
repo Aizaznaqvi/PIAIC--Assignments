23c590f5bdf2c626bc0cef3ed595fc3a1539bb9c
Muhammad Aizaz Haider <106084777+Aizaznaqvi@users.noreply.github.com>
2023-10-21 17:02:33 +0500

    Update and rename Assignment-5 to Assignment5/Solution.ts

---
+++ Assignment5/Solution.ts
@@ -0,0 +1,7 @@
+const nameWithWhiteSpace = "    Aizaz Haider  ";
+console.log("Name With White Space");
+console.log(nameWithWhiteSpace);
+
+const strippedName = nameWithWhiteSpace.trim();
+console.log("Name without white space");
+console.log(strippedName)
