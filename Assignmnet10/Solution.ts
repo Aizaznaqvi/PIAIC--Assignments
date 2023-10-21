db48fa6328d203cb818f63199ffce67b8885f44f
Muhammad Aizaz Haider <106084777+Aizaznaqvi@users.noreply.github.com>
2023-10-21 17:40:39 +0500

    Update and rename Assignmnet-10 to Assignmnet10/Solution.ts

--- Assignmnet10/Solution.ts
+++ Assignmnet10/Solution.ts
@@ -1,5 +1,3 @@
-Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
-
 const favoriteCars :string[] = ["BMW", "Audit Q6", "mercedes", "Ferrari"];
 console.log("I have traveled in" + " "+ favoriteCars[0]);
 console.log("I have traveled in" + " "+ favoriteCars[1]);
