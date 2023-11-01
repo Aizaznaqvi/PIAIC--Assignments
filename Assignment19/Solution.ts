cba795191f53ebba6e5ad5c4b02413b0020a85e3
Muhammad Aizaz Haider <106084777+Aizaznaqvi@users.noreply.github.com>
2023-11-01 13:32:10 +0500

    Create Solution.ts

---
+++ Assignment19/Solution.ts
@@ -0,0 +1,35 @@
+let car = 'Mehran';
+let age = 28;
+let isMale = true;
+let hasLicense = false;
+let firstName = 'Aizaz';
+
+console.log("Test 1: Is car == 'Mehran'? I predict True.");
+console.log(car == 'Mehran');
+
+console.log("Test 2: Is age < 18? I predict False.");
+console.log(age < 18);
+
+console.log("Test 3: Is he Male? I predict True.");
+console.log(isMale);
+
+console.log("Test 4: Does the person have a driver's license? I predict False.");
+console.log(hasLicense);
+
+console.log("Test 5: Is the name == 'Bob'? I predict False.");
+console.log(firstName == 'Naqvi');
+
+console.log("Test 6: Is age >= 21? I predict True.");
+console.log(age >= 21);
+
+console.log("Test 7: Is car != 'ford'? I predict True.");
+console.log(car != 'ford');
+
+console.log("Test 8: Is name == 'Aizaz' || name == 'Naqvi'? I predict True.");
+console.log(firstName == 'Aizaz' || firstName == 'Naqvi');
+
+console.log("Test 9: Is !isMale && hasLicense? I predict False.");
+console.log(!isMale && hasLicense);
+
+console.log("Test 10: Is age < 18 || car != 'ford'? I predict True.");
+console.log(age < 18 || car != 'ford');
